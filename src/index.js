bb1c7a871a1dd78c9fc48346b2d64ae7204cd52a
emrekardaslar <emre.kardaslar@gmail.com>
2023-11-27 21:20:50 +0300

    add example scene

--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Phaser from "phaser";
+/* import logoImg from "./assets/logo.png"; */
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+class MyGame extends Phaser.Scene {
+  platforms;
+  player;
+  constructor() {
+    super();
+  }
 
-    preload ()
-    {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
+  preload() {
+    //  This is an example of a bundled image:
+    /*     this.load.image("logo", logoImg); */
 
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
-    }
-      
-    create ()
-    {
-        this.add.image(400, 300, 'background');
+    //  This is an example of loading a static image from the public folder:
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
 
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  create() {
+    this.add.image(400, 300, "sky");
+    this.add.image(400, 500, "ground");
+    this.add.image(300, 200, "ground");
+    this.add.image(300, 470, "star");
+    this.add.image(400, 460, "dude");
+
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyGame,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
