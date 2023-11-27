76f761a1bc2cce51724700738a5cfb6ecbb36190
emrekardaslar <emre.kardaslar@gmail.com>
2023-11-27 21:32:17 +0300

    add collider and movement

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Phaser from "phaser";
 class MyGame extends Phaser.Scene {
   platforms;
   player;
+  cursors;
+
   constructor() {
     super();
   }
@@ -25,10 +27,7 @@ class MyGame extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, "sky");
-    this.add.image(400, 500, "ground");
-    this.add.image(300, 200, "ground");
     this.add.image(300, 470, "star");
-    this.add.image(400, 460, "dude");
 
     this.platforms = this.physics.add.staticGroup();
 
@@ -37,6 +36,54 @@ class MyGame extends Phaser.Scene {
     this.platforms.create(600, 400, "ground");
     this.platforms.create(50, 250, "ground");
     this.platforms.create(750, 220, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, this.platforms);
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
 
